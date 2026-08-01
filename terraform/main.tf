65bd2739a82b7c88038303da74568694a4b2ae65
Darshak Rakholiya <darshakrakholiya.ca@gmail.com>
2026-08-01 19:37:42 -0400

    Fix app setting name to match app.js env var (AZURE_COSMOS_CONNECTIONSTRING)1

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,14 +64,15 @@ resource "azurerm_key_vault" "main" {
   sku_name                   = "standard"
   soft_delete_retention_days = 7
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    secret_permissions = ["Get", "List", "Set", "Delete", "Purge"]
-  }
-
   tags = var.tags
+}
+
+resource "azurerm_key_vault_access_policy" "current_user" {
+  key_vault_id = azurerm_key_vault.main.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  secret_permissions = ["Get", "List", "Set", "Delete", "Purge"]
 }
 
 resource "azurerm_key_vault_access_policy" "webapp" {
